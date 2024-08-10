633cb52c45181599fa9688c4f2274be401c5b422
mpearce-rsi <pearce.matthewa@gmail.com>
2024-08-10 13:20:10 -0600

    added testimonials collection

--- kaldes-cms/types/generated/components.d.ts
+++ kaldes-cms/types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface BasicStringArray extends Schema.Component {
+  collectionName: 'components_basic_string_arrays';
+  info: {
+    displayName: 'Quotes';
+    icon: 'bulletList';
+    description: '';
+  };
+  attributes: {
+    Quote: Attribute.Text;
+    Name: Attribute.String;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'basic.string-array': BasicStringArray;
+    }
+  }
 }
